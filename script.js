921f184f85aaa820e6943a15d683867e33ba4339
ibrahim fikry <ibrahim.fikry2050@gmail.com>
2022-05-30 14:40:30 +0200

    event-propagation

--- script.js
+++ script.js
@@ -4,20 +4,36 @@ var innerimg = document.getElementById('innerimg')
 var closebtn = document.getElementById('btn-close')
 var rightbtn = document.getElementById('btn-right')
 var leftbtn = document.getElementById('btn-left')
+var fixedboxLayer = document.getElementById('fixedboxLayer')
 var imgcontainer = document.getElementById('imgcontainer')
 imgcontainer.addEventListener('click', (e) => {
 
     var clickedimg = e.target
     var clickedimgsrc = clickedimg.getAttribute('src')
     if (clickedimgsrc != null) {
+
         innerimg.setAttribute('src', clickedimgsrc)
         fixedbox.classList.replace('d-none', 'd-flex')
     }
 
 })
-closebtn.addEventListener('click', () => {
+fixedbox.addEventListener('click', (e) => {
+
     fixedbox.classList.replace('d-flex', 'd-none')
 })
-fixedbox.addEventListener('click', () => {
+
+fixedboxLayer.addEventListener('click', (e) => {
+    e.stopPropagation()
+})
+closebtn.addEventListener('click', (e) => {
+
     fixedbox.classList.replace('d-flex', 'd-none')
+})
+leftbtn.addEventListener('click', (e) => {
+
+
+})
+rightbtn.addEventListener('click', (e) => {
+
+
 })
