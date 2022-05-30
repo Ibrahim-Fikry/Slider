d780ebe657005c1f3c4de2fcb508c2526c838ffb
ibrahim fikry <ibrahim.fikry2050@gmail.com>
2022-05-30 21:15:28 +0200

    sliding with keyboard

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//#region   select elements
 var selectedimg = document.querySelectorAll('#imgcontainer img') //list of image
 var imgarr = Array.from(selectedimg)
 var fixedbox = document.getElementById('fixedbox')
@@ -8,6 +9,9 @@ var leftbtn = document.getElementById('btn-left')
 var fixedboxLayer = document.getElementById('fixedboxLayer')
 var imgcontainer = document.getElementById('imgcontainer')
 var selectedimgindex = 0
+    //#endregion
+
+
 
 imgcontainer.addEventListener('click', (e) => {
 
@@ -21,37 +25,61 @@ imgcontainer.addEventListener('click', (e) => {
     }
 
 })
-fixedbox.addEventListener('click', (e) => {
-
-    fixedbox.classList.replace('d-flex', 'd-none')
-})
 
 fixedboxLayer.addEventListener('click', (e) => {
     e.stopPropagation()
 })
-closebtn.addEventListener('click', (e) => {
+fixedbox.addEventListener('click', closeslid)
+closebtn.addEventListener('click', closeslid)
+leftbtn.addEventListener('click', getprevslider)
+rightbtn.addEventListener('click', getnextslider)
 
-    fixedbox.classList.replace('d-flex', 'd-none')
-})
-leftbtn.addEventListener('click', (e) => {
-    selectedimgindex--
-    if (selectedimgindex < 0) {
-        selectedimgindex = selectedimg.length - 1
-    }
-    rightliftslider()
-
-})
-rightbtn.addEventListener('click', (e) => {
-
+function getnextslider() {
     ++selectedimgindex
     if (selectedimgindex == selectedimg.length) {
         selectedimgindex = 0
     }
     rightliftslider()
-})
+}
+
+function getprevslider() {
+    selectedimgindex--
+    if (selectedimgindex < 0) {
+        selectedimgindex = selectedimg.length - 1
+    }
+    rightliftslider()
+}
 
 function rightliftslider() {
-    var previmg = selectedimg[selectedimgindex]
-    var previmgsrc = previmg.getAttribute('src')
-    innerimg.setAttribute('src', previmgsrc)
+    var slidimg = selectedimg[selectedimgindex]
+    var slidimgsrc = slidimg.getAttribute('src')
+    innerimg.setAttribute('src', slidimgsrc)
 }
+
+function closeslid() {
+    fixedbox.classList.replace('d-flex', 'd-none')
+}
+//-----use   ArrowLeft    ArrowRight   spacebar
+document.addEventListener('keyup', (e) => {
+
+    if (fixedbox.getAttribute('class').includes("d-flex") == true) {
+        if (e.key == "ArrowRight") {
+            ++selectedimgindex
+            if (selectedimgindex == selectedimg.length) {
+                selectedimgindex = 0
+            }
+            rightliftslider()
+
+        } else if (e.key == "ArrowLeft") {
+            --selectedimgindex
+            if (selectedimgindex < 0) {
+                selectedimgindex = selectedimg.length - 1
+            }
+            rightliftslider()
+
+        } else if (e.key == " ") {
+            closeslid()
+        }
+    }
+
+})
