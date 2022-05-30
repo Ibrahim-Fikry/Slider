7b941a466e36b473233c6c0f2c9341ff791a79ae
ibrahim fikry <ibrahim.fikry2050@gmail.com>
2022-05-30 18:17:36 +0200

    getNext&PreviousSlider

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var selectedimg = document.querySelectorAll('#containerimg img') //list of image
+var selectedimg = document.querySelectorAll('#imgcontainer img') //list of image
+var imgarr = Array.from(selectedimg)
 var fixedbox = document.getElementById('fixedbox')
 var innerimg = document.getElementById('innerimg')
 var closebtn = document.getElementById('btn-close')
@@ -6,9 +7,12 @@ var rightbtn = document.getElementById('btn-right')
 var leftbtn = document.getElementById('btn-left')
 var fixedboxLayer = document.getElementById('fixedboxLayer')
 var imgcontainer = document.getElementById('imgcontainer')
+var selectedimgindex = 0
+
 imgcontainer.addEventListener('click', (e) => {
 
     var clickedimg = e.target
+    selectedimgindex = imgarr.indexOf(clickedimg)
     var clickedimgsrc = clickedimg.getAttribute('src')
     if (clickedimgsrc != null) {
 
@@ -30,10 +34,24 @@ closebtn.addEventListener('click', (e) => {
     fixedbox.classList.replace('d-flex', 'd-none')
 })
 leftbtn.addEventListener('click', (e) => {
-
+    selectedimgindex--
+    if (selectedimgindex < 0) {
+        selectedimgindex = selectedimg.length - 1
+    }
+    rightliftslider()
 
 })
 rightbtn.addEventListener('click', (e) => {
 
-
+    ++selectedimgindex
+    if (selectedimgindex == selectedimg.length) {
+        selectedimgindex = 0
+    }
+    rightliftslider()
 })
+
+function rightliftslider() {
+    var previmg = selectedimg[selectedimgindex]
+    var previmgsrc = previmg.getAttribute('src')
+    innerimg.setAttribute('src', previmgsrc)
+}
